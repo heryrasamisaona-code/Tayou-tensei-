2ebb03b10dc64798085e664adc49e10dd1683966
heryrasamisaona-code <heryrasamisaona@gmail.com>
2026-03-02 22:22:29 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,25 @@
-const cacheName = 'tayou-v1';
+const cacheName = 'tayou-v2';
 const assets = [
-  'index.html',
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png',
   'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js',
   'https://unpkg.com/peerjs@1.5.4/dist/peerjs.min.js'
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
